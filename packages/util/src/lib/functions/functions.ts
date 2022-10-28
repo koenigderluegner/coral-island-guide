7fed6444780cbf967fd17f63f50054b99fb46a55
Alexander Niersmann <aniersmann@gmail.com>
2022-10-28 02:41:47 +0200

    removed console.log

--- packages/util/src/lib/functions/functions.ts
+++ packages/util/src/lib/functions/functions.ts
@@ -1,5 +1,6 @@
 export function getEnumValue(EnumString: string): string {
     let strings = EnumString.split('::');
+
     return strings[1] ?? EnumString[0];
 }
 
@@ -9,15 +10,10 @@ export function allTrue(obj: Record<string, boolean>): boolean {
     const trueValues = keys.map(key => obj[key]).filter(b => b);
 
     return trueValues.length === keys.length;
-
 }
 
 
 export function getTruthyValues(obj: Record<string, boolean>, anyText = 'Any'): string {
-
-    console.log(obj, allTrue(obj), Object.keys(obj)
-        .filter((key) => obj[key]),);
-
     return allTrue(obj)
         ? anyText
         : Object.keys(obj)
