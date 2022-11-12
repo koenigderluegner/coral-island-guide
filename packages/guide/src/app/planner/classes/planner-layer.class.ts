065f60e24baaf690ad75591c326466d469f87d22
Alexander Niersmann <aniersmann@gmail.com>
2022-11-12 18:54:37 +0100

    added basic saving and position conflict calculation

--- packages/guide/src/app/planner/classes/planner-layer.class.ts
+++ packages/guide/src/app/planner/classes/planner-layer.class.ts
@@ -1,10 +1,18 @@
+import { GridPlaceable } from '../interfaces/grid-placeable.interface';
+
 export class PlannerLayer {
     protected width: number;
     protected height: number;
+    placeables: { x: number, y: number; item: (GridPlaceable<any> & { key: string }) }[] = [];
 
 
     constructor(width: number, height: number) {
         this.width = width;
         this.height = height;
     }
+
+    addPlaceable(key: string, item: { x: number, y: number; item: (GridPlaceable<any> & { key: string }) }): void {
+        this.placeables.push(item);
+    }
+
 }
