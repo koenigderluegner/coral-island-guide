c6a57cbfb834696aaf76e2be92bd9638d2f0b088
Alexander Niersmann <aniersmann@gmail.com>
2023-07-09 14:43:09 +0200

    added completion animation

--- packages/guide/src/app/checklist/checklist.component.ts
+++ packages/guide/src/app/checklist/checklist.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 import { ChecklistService } from "../core/services/checklist.service";
 import { Checklist } from "../core/interfaces/checklist.interface";
+import { animate, style, transition, trigger } from "@angular/animations";
 
 @Component({
     selector: 'app-checklist',
-    templateUrl: './checklist.component.html'
+    templateUrl: './checklist.component.html',
+    animations: [
+        trigger(
+            'leaveAnimation',
+            [
+                transition(
+                    ':leave',
+                    [
+                        style({height: '*', opacity: 1}),
+                        animate('200ms ease-in',
+                            style({height: 0, opacity: 0}))
+                    ]
+                )
+            ]
+        )
+    ]
 })
 export class ChecklistComponent {
 
