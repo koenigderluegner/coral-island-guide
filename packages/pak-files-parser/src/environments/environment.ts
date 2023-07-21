ea72d226a360bdfb62f45189eb7674e04269ffa8
Alexander Niersmann <aniersmann@gmail.com>
2023-07-21 16:35:09 +0200

    added beta data to parser

--- packages/pak-files-parser/src/environments/environment.ts
+++ packages/pak-files-parser/src/environments/environment.ts
@@ -2,6 +2,6 @@ import { EnvironmentConfig } from "./environment-config.interface";
 import path from "path";
 
 export const environment: EnvironmentConfig = {
-    isBeta: false,
-    assetPath: path.join(__dirname, 'assets', 'live')
+    isBeta: true,
+    assetPath: path.join(__dirname, 'assets', 'beta')
 };
