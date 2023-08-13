eb4ff7963f8f602c719b4113fe2981c814582fc5
Alexander Niersmann <aniersmann@gmail.com>
2023-08-14 00:35:59 +0200

    visual fixes in shop details

--- packages/guide/src/app/locations/components/shop-item-data-details/shop-item-data-details.component.ts
+++ packages/guide/src/app/locations/components/shop-item-data-details/shop-item-data-details.component.ts
@@ -4,7 +4,6 @@ import { ShopItemData } from "@ci/data-types";
 @Component({
     selector: 'app-shop-item-data-details',
     templateUrl: './shop-item-data-details.component.html',
-    styleUrls: ['./shop-item-data-details.component.scss'],
 })
 export class ShopItemDataDetailsComponent {
 
