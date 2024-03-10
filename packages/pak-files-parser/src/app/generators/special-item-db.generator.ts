096bc8ba370d6f43b611095eb6d1c6cc2dc6ae50
Alexander Niersmann <aniersmann@gmail.com>
2024-03-10 15:09:47 +0100

    moved generators

---
+++ packages/pak-files-parser/src/app/generators/special-item-db.generator.ts
@@ -0,0 +1,23 @@
+import { convertToIconName, getReferencedString, readAsset } from '../../util/functions';
+import { SpecialItem } from '@ci/data-types';
+import { BaseGenerator } from "./base-generator.class";
+import { RawSpecialItem } from "../../interfaces/raw-data-interfaces/raw-special-item.interface";
+import { Datatable } from "../../interfaces/datatable.interface";
+import { StringTable } from "../../util/string-table.class";
+
+export class SpecialItemDbGenerator extends BaseGenerator<RawSpecialItem, SpecialItem> {
+
+    datatable: Datatable<RawSpecialItem>[] = readAsset<Datatable<RawSpecialItem>[]>('ProjectCoral/Content/ProjectCoral/Core/Data/ItemSpecial/DT_ItemSpecial.json');
+
+    handleEntry(itemKey: string, dbItem: RawSpecialItem): SpecialItem | undefined {
+
+        return {
+            id: itemKey,
+            displayName: StringTable.getString(dbItem.name) ?? '',
+            description: StringTable.getString(dbItem.description) ?? '',
+            iconName: convertToIconName(getReferencedString(dbItem.icon.ObjectName)).replace('.png', ''),
+        };
+    }
+
+
+}
