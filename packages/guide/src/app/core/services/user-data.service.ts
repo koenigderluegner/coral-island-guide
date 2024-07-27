9aa113eeadf5d2834c8f6c40abaf62eba54c97b7
Alexander Niersmann <aniersmann@gmail.com>
2024-07-27 13:58:03 +0200

    migrate my guide date to settings

--- packages/guide/src/app/core/services/user-data.service.ts
+++ packages/guide/src/app/core/services/user-data.service.ts
@@ -6,7 +6,7 @@ import { UserData } from "../types/user-data.type";
     providedIn: 'root'
 })
 export class UserDataService {
-    private static readonly _CURRENT_USER_DATA_VERSION = 1
+    private static readonly _CURRENT_USER_DATA_VERSION = 2
     private static readonly _USER_DATA_STORE_KEY = 'user-data'
     userData = signal<{ version: number, currentIndex: number; data: UserData[] }>({
         version: UserDataService._CURRENT_USER_DATA_VERSION,
@@ -23,7 +23,17 @@ export class UserDataService {
     read(): void {
         const userData = localStorage.getItem(UserDataService._USER_DATA_STORE_KEY + this.#versionSuffix);
         if (userData) {
-            this.userData.set(JSON.parse(userData));
+
+            const parsedJSON = JSON.parse(userData);
+            const migrated = this.#migrate(parsedJSON);
+
+            this.userData.set(migrated);
+
+            if (parsedJSON.version !== migrated.version) {
+                this.save();
+            }
+
+
         } else {
             this.userData.set({
                 version: UserDataService._CURRENT_USER_DATA_VERSION,
@@ -36,7 +46,35 @@ export class UserDataService {
 
     createEmptyUserData(): UserData {
         return {
+            myGuideDate: {year: 1, day: 1, season: "Spring"},
             todos: []
         }
+    }
+
+    #migrate(userData: any): { version: number, currentIndex: number; data: UserData[] } {
+        let migratedData: UserData[] = userData.data ?? [];
+        let existingVersion = userData.version
+        while (existingVersion !== UserDataService._CURRENT_USER_DATA_VERSION) {
+
+            if (!existingVersion) {
+                migratedData = [this.createEmptyUserData()];
+                existingVersion = UserDataService._CURRENT_USER_DATA_VERSION;
+            } else if (existingVersion === 1) {
+                migratedData = migratedData.map(d => {
+                    d.myGuideDate = {year: 1, day: 1, season: "Spring"};
+                    return d
+                });
+                existingVersion = 2;
+            }
+
+
+        }
+
+        return {
+            version: UserDataService._CURRENT_USER_DATA_VERSION,
+            currentIndex: userData.currentIndex ?? 0,
+            data: migratedData
+        }
+
     }
 }
--- packages/guide/src/app/core/types/user-data.type.ts
+++ packages/guide/src/app/core/types/user-data.type.ts
@@ -1,5 +1,7 @@
 import { ToDo } from "./to-do.type";
+import { SpecificDate } from "@ci/data-types";
 
 export type UserData = {
+    myGuideDate: SpecificDate;
     todos: ToDo[]
 }
