25b011a0aaaf64e93876a878304d23eccffbba26
Alexander Niersmann <aniersmann@gmail.com>
2025-02-06 18:00:12 +0100

    executed lazy load migration of routes

--- packages/guide/src/app/crafting/crafting-routing.module.ts
+++ packages/guide/src/app/crafting/crafting-routing.module.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { CraftingComponent } from './crafting.component';
-import { InventoryComponent } from './components/inventory/inventory.component';
-import { ProcessorComponent } from './components/processor/processor.component';
-import { CookingComponent } from "./components/cooking/cooking.component";
+
+
+
+
 
 export const routes: Routes = [
     {
@@ -12,13 +12,13 @@ export const routes: Routes = [
     },
     {
         path: '',
-        component: CraftingComponent,
+        loadComponent: () => import('./crafting.component').then(m => m.CraftingComponent),
         children: [
-            {path: 'inventory', component: InventoryComponent, title: 'Inventory - Crafting'},
+            {path: 'inventory', loadComponent: () => import('./components/inventory/inventory.component').then(m => m.InventoryComponent), title: 'Inventory - Crafting'},
             {path: 'artisan', redirectTo: 'artisan/', pathMatch: 'full'},
-            {path: 'artisan/:tabName', component: ProcessorComponent, title: 'Artisan - Crafting'},
+            {path: 'artisan/:tabName', loadComponent: () => import('./components/processor/processor.component').then(m => m.ProcessorComponent), title: 'Artisan - Crafting'},
             {path: 'cooking', redirectTo: 'cooking/', pathMatch: 'full'},
-            {path: 'cooking/:tabName', component: CookingComponent, title: 'Cooking - Crafting'},
+            {path: 'cooking/:tabName', loadComponent: () => import('./components/cooking/cooking.component').then(m => m.CookingComponent), title: 'Cooking - Crafting'},
             {
                 path: 'mixing',
                 loadComponent: () => import('./mixing/mixing.component').then(c => c.MixingComponent),
